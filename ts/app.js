e7d750b3d2f9ef5e8b20e0b2de42fce186836041
spwilliams2002 <94202523+spwilliams2002@users.noreply.github.com>
2024-12-17 00:08:24 -0700

    ughh

--- ts/app.js
+++ ts/app.js
@@ -1,1 +1,29 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const getUsers = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const response = yield fetch('https://tangentbackend.fly.dev', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = yield response.json();
+        console.log(data);
+    }
+    catch (error) {
+        console.error('Error fetching users:', error);
+    }
+});
+getUsers();
---
+++ ts/app.ts
@@ -0,0 +1,19 @@
+const getUsers = async () => {
+  try {
+    const response = await fetch('https://tangentbackend.fly.dev', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('Error fetching users:', error);
+  }
+};
+
+getUsers();
